de51ffa330672921208da6182117c4812b736081
yoko <yoko.hashimoto.29@gmail.com>
2018-12-12 08:54:46 +0900

    ログイン機能を修正した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   def create 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render 'new'
-      # redirect_to new_session_path
     end
   end
 end
